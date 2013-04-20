19115b28906f1f2fa53cb2ae23ee87edec82c660
Keyan Zhao <kyzhao@gmail.com>
2013-04-19 17:23:34 -0700

    Added the R code for fast read in large plink files into R

---
+++ readplinkgeno.R
@@ -0,0 +1,30 @@
+## suppose you have plinkfileprefix.ped plinkfileprefix.map for genotypes.
+
+tmpgenoprefix <- "plinkLDpruned_allchrs" ## as example
+
+plinkcmmd <- paste ("plink --noweb", tmpgenoprefix,  "--recodeA --out ", tmpgenoprefix )
+
+## first run the following 3 lines on command line: 
+# plink --noweb --file  plinkLDpruned_allchrs  --recodeA --out plinkLDpruned_allchrs
+#cut -d" " -f1-6 plinkLDpruned_allchrs.raw > plinkLDpruned_allchrs.fam
+#cut -d" " -f7- plinkLDpruned_allchrs.raw > plinkLDpruned_allchrs.raw0
+
+## then run this R script to scan in the genotype and save as RData. 
+
+tmpgenofile <- paste(tmpgenoprefix,".raw0",sep="")
+tmpmapfile <- paste(tmpgenoprefix,".map",sep="")
+
+tmpfamfile <- paste(tmpgenoprefix,".fam",sep="")
+map <- as.matrix(read.csv(tmpmapfile, header=F,sep="\t"))
+fam <- read.table(tmpfamfile, header=T,sep=" ")
+dim(fam)
+ninds <- dim(fam)[1]
+colnames(map) <- c("Chr","SNPID","cM","Pos")
+nsnps <- dim(map)[1]
+
+geno <- matrix(scan( tmpgenofile, n = ninds*nsnps,skip=1), ninds, nsnps, byrow = TRUE)
+save(geno,map,fam, file= paste(tmpgenoprefix,".RData",sep=""), compress=FALSE)
+
+## later on, to use the genotype, simply load the .RData.
+#load( paste(tmpgenoprefix,".RData",sep=""))
+
